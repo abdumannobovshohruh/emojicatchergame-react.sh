ed7421678863adaf174aea013092a8b4fae13f6a
shohruh973 <abdumannobovshohruh973@gmail.com>
2023-05-27 17:43:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useRef,useEffect, useState } from 'react';
+import { Element } from './utils/Constants';
+import './App.css'
 function App() {
+   const gridRef = useRef()
+   const [classListName , setClassListName] = useState()  
+   const [count,setCount] = useState(0)
+  const RandomFunc = ()=>{
+    return Math.round(Math.random()*9+1);
+  }
+
+ 
+  setTimeout(()=>{
+    setClassListName(RandomFunc())
+  },500)  
+    
+ 
+
+  const handlerClick = (item)=>{
+    if(classListName ==  item.target.id){
+      setCount(p=>++p);
+    }
+    console.log(item.target);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="scores-container">
+      <div className="total-score">
+        <h2>Your Score:</h2>
+        <h2 id="score">{count}</h2>
+      </div>
+
+      <div className="time">
+        <h2>Time left:</h2>
+        <h2 id="time-left">60</h2>
+      </div>
+    </div>
+
+    <div className="grid-container">
+      <div ref={gridRef} onClick={(e)=>handlerClick(e)} className="grid">
+        {Element.map((value)=>{
+          return  <div key={value.id} className={ classListName == value.id ?  `${value.list} emoji` : `${value.list}`  } id={value.id}></div>
+      })}  
+        
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/utils/Constants.js
@@ -0,0 +1,44 @@
+
+
+export const Element  = [
+    {
+        id:1,
+        list:"square"
+    },
+    {
+        id:2,
+        list:"square"
+    },
+    {
+        id:3,
+        list:"square"
+    },
+    {
+        id:4,
+        list:"square"
+    },
+    {
+        id:5,
+        list:"square"
+    },
+    {
+        id:6,
+        list:"square"
+    },
+    {
+        id:7,
+        list:"square"
+    },
+    {
+        id:8,
+        list:"square"
+    },
+    {
+        id:9,
+        list:"square"
+    },
+    {
+        id:10,
+        list:"square"
+    },
+]
